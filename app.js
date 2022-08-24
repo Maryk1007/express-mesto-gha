c301b3e5fbedc645454423c2ab9a78f325b7ba91
Maria <mary-sberbank@yandex.ru>
2022-08-24 08:44:56 +0300

    config eslint

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const { PORT = 3001 } = process.env;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({foo: 'Hello world!'})
-})
+  res.send({ foo: 'Hello world!' });
+});
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server listen on ${PORT}`);
 });
