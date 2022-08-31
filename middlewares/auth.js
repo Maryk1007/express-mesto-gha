a29aeea92d72217338f8da57838424bb8392817c
Maria <mary-sberbank@yandex.ru>
2022-08-31 17:32:56 +0300

    2345678765432

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,31 @@
 const jwt = require('../helpers/jwt');
 const { SECRET_KEY } = require('../helpers/jwt');
-// const User = require('../models/userModel');
-
+const User = require('../models/userModel');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(UnauthorizedError).send({ message: 'Необходима авторизация' });
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
-
   const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    return res.status(UnauthorizedError).send({ message: 'Необходима авторизация' });
+    next(new UnauthorizedError('Необходима авторизация'));
   }
-
   req.user = payload;
-  next();
+  User
+    .findOne({ email: payload.email })
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Необходима авторизация');
+      }
+      req.user = { _id: user._id };
+      res.status(200);
+      next();
+    })
+    .catch(next);
 };
